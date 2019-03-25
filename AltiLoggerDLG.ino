b9705debe15b6473d825cf21202b67f62fa979ac
hejnas <hejnas@gmail.com>
2019-03-25 23:39:22 +0100

    do uporzatkowania

--- AltiLoggerDLG.ino
+++ AltiLoggerDLG.ino
@@ -1,24 +1,16 @@
 #include <SPI.h>
 #include "alti_logger.h"
 
-#define altiLogger_ID "DLG_LOGGER_20090605"
-String verLogger = "v0.2";
-#define altiLogger_file_extension ".csv"
-
-#define Flash_CMD_page_program  0x02
-#define Flash_CMD_read_data     0x03
-#define Flash_max_addres        4194303   // 32Mbit = 4Mbyte = 4 x 1024 x 1024 = 4194304 max adress 419304 - 1
-#define Flash_FAT_0_addres      32
-#define Flash_data_0_addres     64
+const String altiLogger_ID = "DLG_LOGGER_20090605";
+const String altiLogger_ver = "v0.2";
+const String altiLogger_file_extension = ".csv";
 
 boolean zatrzymaj = false;
 // Variables:
 int previousMillis = 0;        // will store the last time the LED was updated
-int interval = 1000;            // interval at which to blink (in milliseconds)
+int interval = 1000;           // interval at which to blink (in milliseconds)
 
-uint32_t FlashAddres;  //pierwsze 32 bajty to licznik plików
-double timeCount = 0;   //licznik milisecund od power on
-String input;
+uint32_t FlashAddres;
 
 uint8_t strona[256];
 uint8_t costam = 0;
@@ -32,15 +24,33 @@ uint8_t costam = 0;
 void setup() 
 {
   
-  initLogger();
+  uint8_t i;
+  
+  // initialize alitLogger
+  i = altiLogger_init();
+  if(i != 0) altiLogger_error(i);
+  
   //if (sprawdzMode()==1) modePC(); //connection to PC
-  delay(500);
+  delay(100);
   Serial.begin(SERIAL_BAUD);
-  delay(500);
-  FlashAddres = Flsh_Find_Start_Adress();
+  delay(200);
+  
+  // find first free data Flash addres
+  FlashAddres = Flsh_start();
+  if(FlashAddres == 0 )         //"Flash mem is not formatted"
+  {
+    if(altiLogger_error(3)==true)
+    {
+      modePC();
+    }
+    else altiLogger_error(100); //wrong altiLogger_error return value
+  }
+  
+  ///*
   Serial.print("*****");
   Serial.print(FlashAddres);
   Serial.println("*****");
+  //*/
   
 }//end of setup
 
@@ -61,9 +71,9 @@ void loop()
     previousMillis = previousMillis * interval;
 
     readParameters(); //read MS5611 parameters
-    zmienna = daneDoFlash(); //stwórz ciąg z danych
+    zmienna = measurement_to_FlashStringFormat(); //stwórz ciąg z danych
     Serial.print(zmienna); //wyświetl ciąg
-    FlashAddres = writeFlashString(FlashAddres,zmienna); //zapisz ciąg do Flash
+    //FlashAddres = writeFlashString(FlashAddres,zmienna); //zapisz ciąg do Flash
     
     if (Serial.available() > 0) { //sprawdz czy nie ma komendy
       input = Serial.readString();
@@ -71,7 +81,7 @@ void loop()
     }
     
     timeCount = timeCount + interval; //przelicz daną "czas"
-    
+    LEDping(1);
   }
   
 }//end of loop
@@ -79,19 +89,79 @@ void loop()
 
 // ---------------------------------------------------
 //
+//      Flsh_print_mem - print all write mem
+//
+// ---------------------------------------------------
+void Flsh_print_mem()
+{
+  
+  // print first 64 mem byte
+  Serial.println("\r\n------------------------------");
+  while(FlashBusy());
+  selectFlash();
+    
+  SPI.transfer(Flash_CMD_read_data);
+  SPI.transfer(0x00);
+  SPI.transfer(0x00);
+  SPI.transfer(0x00);
+  uint8_t j = 0;
+  for(uint32_t i = 0; i < Flash_FAT_0_addres; i++)
+  {
+    Serial.write(SPI.transfer(0x00));
+    if(j==15){
+      Serial.println("");
+      j = 0;
+    }
+    else j++;
+  }
+  Serial.println("------------------------------");
+  j = 0;
+  for(uint32_t i = 0; i < Flash_FAT_0_addres; i++)
+  {
+    Serial.write(SPI.transfer(0x00));
+    if(j==15){
+      Serial.println("");
+      j = 0;
+    }
+    else j++;
+  }
+  Serial.println("------------------------------");
+  deselectFlash();
+
+  // print all write memory
+  
+  
+  
+}//end of Flsh_print_mem
+
+
+// ---------------------------------------------------
+//
 //      Flsh_Find_Start_Adress & write new file name
 //
 // ---------------------------------------------------
-uint32_t Flsh_Find_Start_Adress()
+uint32_t Flsh_start()
 {
   
-  uint8_t i = 0;
   uint32_t addr;
+  String ciag1 = "";
+  String ciag2 = "";
 
   uint32_t return_value;
   String nazwa_pliku = "(1" + altiLogger_file_extension + ")";
+
+  // porównanie altiLogger_ID z zapisem na początku pamięci
+  for(addr = 0; addr < altiLogger_ID.length(); addr++)
+  {
+    ciag1 = ciag1 + Flash_read_8bit(addr);
+    ciag2 = ciag2 + uint8_t(altiLogger_ID[addr]);
+  }
+  if(ciag1 != ciag2) return 0;
   
-  if(Flash_read_8bit(0x00)==0xFF){
+  Serial.print(altiLogger_ID + " ");
+  Serial.println(altiLogger_ID.length());
+  
+  /*if(Flash_read_8bit(0x00)==0xFF){
     FlashErase();
     Flash_write_8bit(0x00,0x01);
     return_value = writeFlashString(Flash_data_0_addres, nazwa_pliku);
@@ -99,76 +169,20 @@ uint32_t Flsh_Find_Start_Adress()
   }
   else {
 
-//nazwa nowego pliku
+    //nazwa nowego pliku
     addr = i / 8;
     while(bitRead(Flash_read_8bit(addr),(i - addr * 8)) != 1){
       i++;
       addr = i / 8;
     }
-    nazwa_pliku = "(" + String(i) + altiLogger_file_extension + ")";
+    nazwa_pliku = "(" + String(i) + altiLogger_file_extension + ")";*/
 
-//znajdz start adress
+  //znajdz start adress
 
-    addr = Flash_data_0_addres;
-    while(Flash_read_8bit(addr) != 0xFF){
-      addr++;
-    }
-    return_value = writeFlashString(addr, nazwa_pliku);
-    return return_value;
-  }
-
-  return 0;
+  return 64;
+  
   
 }//end of Flsh_Find_Start_Adress
-
-// ---------------------------------------------------
-//
-//      zwraca zformatowwane dane do zapisu do Flash
-//
-// ---------------------------------------------------
-String daneDoFlash()
-{
-  
-  double zmienna = 0;
-  String dane = "";
-
-  zmienna = timeCount;
-  zmienna = zmienna/1000;
-  dane = String(zmienna,3) + ";";
-  zmienna = Temperature;
-  zmienna = zmienna / 10;
-  dane = dane + String(zmienna,1) + ";";
-  zmienna = Altitude;
-  zmienna = zmienna / 10;
-  dane = dane + String(zmienna,1) + "\r\n";
-
-  return dane;
-  
-}
-
-
-// ---------------------------------------------------
-//
-//      check PC connection
-//
-// ---------------------------------------------------
-uint8_t sprawdzMode()
-{
-  
-  uint16_t x = 0xB4A3;
-  
-  pinMode(serial_TX1, OUTPUT);
-  pinMode(serial_RX1, INPUT_PULLUP);
-  digitalWrite(pinLED, LOW);
-  
-  for (int8_t i=0;i<16;i++)
-  {
-    digitalWrite(serial_TX1, bitRead(x,i));
-    if(digitalRead(serial_RX1)!=bitRead(x,i)) return 1;
-  }
-  return 0;
-  
-}//end of sprawdzMode()
 
 
 // ---------------------------------------------------
@@ -182,7 +196,7 @@ void modePC()
   Serial.begin(SERIAL_BAUD);
   delay(200);
   Serial.print("...::: DLG LOGGER SPI MS5611 ");
-  Serial.print(verLogger);
+  Serial.print(altiLogger_ver);
   Serial.println(" :::...");
   Serial.println("...:::          PC MODE           :::...");
 
@@ -221,30 +235,11 @@ void modePC()
       if (input.indexOf("erase") > -1)FlashErase();
       if (input.indexOf("identyfikator") > -1)Serial.print(altiLogger_ID);
       if (input.indexOf("help") > -1)showLoggerHelp();
+      if (input.indexOf("print") > -1)Flsh_print_mem();
     }
   }
   
 }//end of modePC
-
-
-// ---------------------------------------------------
-//
-//      showLoggerHelp
-//
-// ---------------------------------------------------
-void showLoggerHelp(){
-  
-  Serial.print("DLG LOGGER ");
-        Serial.println("v0.2");
-        Serial.println("lista komend:");        
-        Serial.println("   start - wyświetlania pomiarów");
-        Serial.println("   stop - wyświetlania pomiarów");
-        Serial.println("   read - ...");
-        Serial.println("   erase - kasuj pamięć flash");
-        Serial.println("   identyfikator - wyświatl");
-        Serial.println("   help - to menu");
-  
-}//end of showLoggerHelp
 
 
 // ---------------------------------------------------
@@ -306,61 +301,6 @@ void wyswietl_liczbe(uint8_t liczba)
 
 // ---------------------------------------------------
 //
-//      Flash_read_8bit
-//
-// ---------------------------------------------------
-uint8_t Flash_read_8bit(uint32_t addr)
-{
-  
-  while(FlashBusy());
-
-  uint8_t return_value;
-  uint8_t byteH = addr >> 16;
-  uint8_t byteM = addr >> 8;
-  uint8_t byteL = addr;
-
-  selectFlash();
-  SPI.transfer(Flash_CMD_read_data);
-  SPI.transfer(byteH);
-  SPI.transfer(byteM);
-  SPI.transfer(byteL);
-  return_value = SPI.transfer(0x00);
-  deselectFlash();
-  
-  return return_value;
-  
-}//end of Flash_read_8bit
-
-
-// ---------------------------------------------------
-//
-//      Flash_write_8bit
-//
-// ---------------------------------------------------
-void Flash_write_8bit(uint32_t addr, uint8_t data)
-{
-  
-  while(FlashBusy());
-
-  uint8_t byteH = addr >> 16;
-  uint8_t byteM = addr >> 8;
-  uint8_t byteL = addr;
-
-  Flash_Write_Enable();
-  selectFlash();
-  SPI.transfer(Flash_CMD_page_program);
-  SPI.transfer(byteH);
-  SPI.transfer(byteM);
-  SPI.transfer(byteL);
-  SPI.transfer(data);
-  deselectFlash();
-  Flash_Write_Disable();
-  
-}//end of Flash_write_8bit
-
-
-// ---------------------------------------------------
-//
 //      readFlashPage
 //
 // ---------------------------------------------------
@@ -391,30 +331,3 @@ void readFlashPage(uint16_t pageNum)
   deselectFlash();
   
 }//end of readFlashPage
-
-
-// ---------------------------------------------------
-//
-//      writeFlashString
-//      zapisuje pod wskazany adres flash ciąg
-//      i zwraca nowy dres flash
-//
-// ---------------------------------------------------
-uint32_t writeFlashString(uint32_t addr, String str){
-  
-  while(FlashBusy());
-  Flash_Write_Enable();
-  selectFlash();
-  SPI.transfer(Flash_CMD_page_program);       //Flash page program
-  SPI.transfer(addr>>16);                 //High byte
-  SPI.transfer(addr>>8);                  //Middle byte
-  SPI.transfer(addr);                     //Low byte
-  for(int i = 0; i < str.length(); i++){  //write str to Flash
-    SPI.transfer(str.charAt(i));
-  }
-  deselectFlash();
-  Flash_Write_Disable();
-  addr = addr + str.length(); //przelicz adres
-  return addr;
-  
-}//end of writeFlashString
